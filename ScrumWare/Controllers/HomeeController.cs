113de6c5b4d84e59b55b8cb7e58a09209446a229
Meriameel <elharizimeriame@gmail.com>
2018-05-26 16:32:45 +0100

    Add files via upload

---
+++ ScrumWare/Controllers/HomeeController.cs
@@ -0,0 +1,19 @@
+﻿
+using ScrumWare.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ScrumWare.Controllers
+{
+    public class HomeeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+       
+    }
+}
